2293c351a90171a586f32fde6f5b84a32dc2585d
Jari Hartikainen <jihartikainen@hotmail.com>
2019-06-02 16:23:33 +0300

    by script

--- osa3/puhelinluettelo_ja_tietokanta/index.js
+++ osa3/puhelinluettelo_ja_tietokanta/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* Jari Hartikainen, 30.5.2019 */
 /* Aalto University, Course: Full Stack Web Development, Part 3: puhelinluettelo ja tietokanta step1 ... step8*/
 
@@ -12,10 +13,6 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 const logger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
   next()
 }
 //Express tarkastaa GET-tyyppisten HTTP-pyyntöjen yhteydessä ensin löytyykö pyynnön polkua vastaavan nimistä tiedostoa hakemistosta build //
@@ -50,7 +47,7 @@ app.get('/', (request, response, next) => {
   Person.find({}).then(list=> {
     response.json(list.map(person => person.toJSON()))
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 // get phonebook data from Mongo database in collections "persons", when user enters to ./api/persons web-page //
@@ -58,13 +55,13 @@ app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(list => {
     response.json(list.map(person => person.toJSON()))
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 // User wants to add a new person to Mongo database collection "persons"
 app.post('/api/persons', (request,response, next) => {
   const body = request.body
-  if (body.name === "") {
+  if (body.name === '') {
     response.status(400).json({ error: 'content missing' }).end()
   }
   else {
@@ -75,11 +72,9 @@ app.post('/api/persons', (request,response, next) => {
 
     person.save()
       .then(savedPerson => {
-        console.log("savedPerson....",savedPerson)
         response.json(savedPerson.toJSON())
       })
       .catch(error => {
-        console.log("error in savedPerson....",error)
         next(error)
       })
   }
@@ -87,7 +82,7 @@ app.post('/api/persons', (request,response, next) => {
 
 // User wants to delete person from the phonebook
 app.delete('/api/persons/:id', (request, response, next) => {
-Person.findByIdAndRemove(request.params.id)
+  Person.findByIdAndRemove(request.params.id)
     .then(result => {
       if (result==null) {
         response.status(400).json({ error: 'Data already removed' }).end()
@@ -96,18 +91,18 @@ Person.findByIdAndRemove(request.params.id)
         response.status(204).end()
       }
     })
-  .catch(
-    error => next(error)
-  )
+    .catch(
+      error => next(error)
+    )
 })
 
 // User wants to list one person information from the phonebook
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
-     if (person) {
+      if (person) {
         response.json(person.toJSON())
-        } else {
+      } else {
         response.status(404).end() 
       }
     })
@@ -137,16 +132,15 @@ app.get('/api/info', (request, response, next) => {
 
   Person.find({}).then (
     results => {
-    var collectionElements = results.map(count => count.id).length
-    response.send(`<p>&nbsp;Puhelinluettelossa ${collectionElements} henkilön tiedot<br> <br>&nbsp; ${Date()}</p>`)
-    response.status(204).end()
+      var collectionElements = results.map(count => count.id).length
+      response.send(`<p>&nbsp;Puhelinluettelossa ${collectionElements} henkilön tiedot<br> <br>&nbsp; ${Date()}</p>`)
+      response.status(204).end()
     })
-  .catch(error => next(error)
-  )
+    .catch(error => next(error)
+    )
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.log("error.name in errorHandler",error.name)
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
